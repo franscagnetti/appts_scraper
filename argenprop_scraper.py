b320f78c5667e52080c6baedb42c82c3e6114864
Franco Scagnetti <franscagnetti@gmail.com>
2021-11-29 13:57:12 -0300

    merge both scripts and fix variables for apartments

--- argenprop_scraper.py
+++ argenprop_scraper.py
@@ -1,73 +1,61 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[106]:
-
-
 from bs4 import BeautifulSoup
 import requests
 import re
 
 
-# In[113]:
-
-
 def get_soup(user_agent,site):
     """
-        devuelve objeto soup para la url del argumento, parseada con lxml
+        Returns soup object parsed with lxml
     """
     header = {'user-agent':user_agent}
     site_gotten = requests.get(site,headers=header).text
     soup = BeautifulSoup(site_gotten,'lxml')
     return soup
 
-
-# In[140]:
-
-
-def get_apartment_list(soup_list,price):
+def get_apartment_list(soup):
     """
         Devuelve lista de departamentos -en soup_list- si son menores al precio que viene como argumento.
         TODO -> filtrar o generar tipo de cambio si es USD
         TODO -> cambiar a diccionario para agregar precio
         TODO -> pensar si agregar más filtros
     """
+
+
     apartment_list = []
-    for item in soup_list:
-        try:
-            precio_class = re.findall(r'\d+',item.find('p',class_='card__price').text)
-            precio = ''.join(precio_class)
-        except:
-            precio = '50000000'
-        try:
-            expensas_class = re.findall(r'\d+',item.find('p',class_='card__expenses').text)
-            expensas = '0' if expensas_class is None else ''.join(expensas_class)
-        except:
-            expensas = '0'
-        if(precio):
-            if(int(precio) + int(expensas) < price):
-                for a in item.find_all('a',href=True):
-                    if('Descartados' not in a['href']):
-                        apartment_list.append(f'https://www.argenprop.com{a["href"]}')
-                        
+
+    # TODO: first we work with 1 appt, after we define it we can move to a for iteration for all the appartments
+    # TODO: add more variables to the appartment
+    apartment = {}
+
+    appt = soup.find('div', class_ = 'listing__item')
+    card_details = appt.find('div', class_ = 'card__details-box')
+    card_details_top = card_details.find('div', class_ = 'card__details-box-top')
+
+    card_cost = card_details_top.find('div', class_ = 'card__monetary-values').find('p', class_ = 'card__price').text.strip()
+    card_cost = re.findall('\d*\.?\d+',card_cost)[0]
+
+    card_currency = card_details_top.find('div', class_ = 'card__monetary-values').find('p', class_ = 'card__price').find('span', class_ = 'card__currency').text.strip()
+
+    apartment['card_cost'] = card_cost
+    apartment['card_currency'] = card_currency
+    apartment_list.append(apartment)
+
     return apartment_list
 
-
-# In[141]:
-
-
-if __name__ == "__main__":
-    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36 Edg/96.0.1054.34'
-    site = 'https://www.argenprop.com/departamento-alquiler-barrio-belgrano'
-    belgrano_soup = get_soup(user_agent,site)
-    belgrano_items = belgrano_soup.find_all('div',class_='listing__item')
-    lista_depas = get_apartment_list(belgrano_items,80000)
-    for item in lista_depas:
-        print(item)
-
-
-# In[ ]:
-
-
-
-
+    # for item in soup_list:
+    #     appartment = {}
+    #     try:
+    #         precio_class = re.findall(r'\d+',item.find('p',class_='card__price').text)
+    #         precio = ''.join(precio_class)
+    #     except:
+    #         precio = '50000000'
+    #     try:
+    #         expensas_class = re.findall(r'\d+',item.find('p',class_='card__expenses').text)
+    #         expensas = '0' if expensas_class is None else ''.join(expensas_class)
+    #     except:
+    #         expensas = '0'
+    #     if(precio):
+    #         if(int(precio) + int(expensas) < price):
+                # for a in item.find_all('a',href=True):
+                #     if('Descartados' not in a['href']):
+                #         apartment_list.append(f'https://www.argenprop.com{a["href"]}')
--- main.py
+++ main.py
@@ -5,23 +5,32 @@ from bs4 import BeautifulSoup
 import requests
 from requests.api import head
 import re
+import argenprop_scraper as aps
 
-
-url = 'https://www.argenprop.com/inmuebles-pais-argentina'
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
-html_text = requests.get(url, headers=header).text
 
 # TODO: add class_ listing__item listing__item--featured
 
-soup = BeautifulSoup(html_text, 'lxml')
-appt = soup.find('div', class_ = 'listing__item')
-card_details = appt.find('div', class_ = 'card__details-box')
-card_details_top = card_details.find('div', class_ = 'card__details-box-top')
+# appt = soup.find('div', class_ = 'listing__item')
+# card_details = appt.find('div', class_ = 'card__details-box')
+# card_details_top = card_details.find('div', class_ = 'card__details-box-top')
 
-card_cost = card_details_top.find('div', class_ = 'card__monetary-values').find('p', class_ = 'card__price').text.strip()
-card_cost = re.findall('\d*\.?\d+',card_cost)[0]
+# card_cost = card_details_top.find('div', class_ = 'card__monetary-values').find('p', class_ = 'card__price').text.strip()
+# card_cost = re.findall('\d*\.?\d+',card_cost)[0]
 
-card_currency = card_details_top.find('div', class_ = 'card__monetary-values').find('p', class_ = 'card__price').find('span', class_ = 'card__currency').text.strip()
+# card_currency = card_details_top.find('div', class_ = 'card__monetary-values').find('p', class_ = 'card__price').find('span', class_ = 'card__currency').text.strip()
 
-print(card_currency)
-print(card_cost)
+# print(card_currency)
+# print(card_cost)
+
+if __name__ == "__main__":
+    #set soup
+    user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'
+    site = 'https://www.argenprop.com/inmuebles-pais-argentina'
+
+    soup = aps.get_soup(user_agent,site)
+
+    appts_list = aps.get_apartment_list(soup)
+
+    print("Returning all the appartments found")
+    for item in appts_list:
+        print(item)
