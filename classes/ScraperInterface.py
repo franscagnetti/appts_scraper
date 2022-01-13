c88c15d69bca1135b5b57089e2d20fc2bbc895c8
Franco Scagnetti <franscagnetti@gmail.com>
2022-01-13 00:36:37 -0300

    add class Scraper and argenprop Scraper

---
+++ classes/ScraperInterface.py
@@ -0,0 +1,143 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+from abc import ABC, abstractmethod
+from .Property import Property
+
+class ScraperInterface(ABC):
+
+  def __init__(self):
+        super().__init__()
+
+  def get_soup(self, user_agent: str, site: str):
+      """
+          Returns soup object parsed with lxml
+      """
+      header = {'user-agent':user_agent}
+      site_gotten = requests.get(site,headers=header).text
+      soup = BeautifulSoup(site_gotten,'lxml')
+      return soup
+
+  @abstractmethod
+  def get_property_list(self, soup, user_agent: str):
+    """
+        Returns a list of properties in json format.
+    """
+    pass
+
+
+class ArgenpropScraper(ScraperInterface):
+
+  def get_property_list(self, soup,user_agent):
+      """
+          Returns a list of properties in json format.
+      """
+      
+      # Looping up to max pages
+      max_pages = 1
+      counter = 0
+
+      property = {}
+      properties_found_list = []
+
+      while True:
+          print(f'Looping pages up to number {max_pages+1}')
+          print(f'Page : {counter+1}')
+          
+          prop_list = soup.find_all('div', class_ = 'listing__item')
+
+          for prop in prop_list:
+              property_obj = Property()
+
+              # Card details
+              card_details = prop.find('div', class_ = 'card__details-box')
+
+              # Card details top
+              card_details_top = card_details.find('div', class_ = 'card__details-box-top')
+
+              try:
+                  card_cost = card_details_top.find('div', class_ = 'card__monetary-values').find('p', class_ = 'card__price').text.strip()
+                  card_cost = re.findall('\d*\.?\d+',card_cost)[0]
+                  property_obj.cost = card_cost
+              except:
+                  pass
+
+              try:
+                  card_currency = card_details_top.find('div', class_ = 'card__monetary-values').find('p', class_ = 'card__price').find('span', class_ = 'card__currency').text.strip()
+                  property_obj.currency = card_currency
+              except:
+                  pass
+
+              try:
+                  card_expenses = card_details_top.find('div', class_ = 'card__monetary-values').find('p', class_ = 'card__expenses').text.strip()
+                  card_expenses_refactored = re.findall("\d+", re.sub('[^A-Za-z0-9]+', '', card_expenses))[0]
+                  property_obj.expenses = card_expenses_refactored
+              except:
+                  pass
+
+              try:
+                  main_features = card_details_top.find('div', class_ = 'card__monetary-values').find('ul', class_ = 'card__main-features').find_all('li')
+                  for feature in main_features:
+                      if "icono-superficie_cubierta" in str(feature):
+                          property_obj.covered_area = feature.find('span').text.strip()
+                      elif "icono-cantidad_dormitorios" in str(feature):
+                          property_obj.number_of_rooms = feature.find('span').text.strip()
+                      elif "icono-antiguedad" in str(feature):
+                          property_obj.antiquity = feature.find('span').text.strip()
+                      elif "icono-cantidad_banos" in str(feature):
+                          property_obj.number_of_baths = feature.find('span').text.strip()
+              except:
+                  pass
+
+              try:
+                  card_address = card_details.find('h2', class_ = 'card__address').text.strip()
+                  property_obj.address = card_address
+              except:
+                  pass
+
+              try:
+                  card_title_primary = card_details.find('p', class_ = 'card__title--primary').text.strip()
+                  property['card_title_primary'] = card_title_primary
+              except:
+                  pass
+
+              try:
+                  card_title = card_details.find('p', class_ = 'card__title').text.strip()
+                  property['card_title'] = card_title
+              except:
+                  pass
+
+              try:
+                  card_info = card_details.find('p', class_ = 'card__info ').text.strip()
+                  property['card_info'] = card_info
+              except:
+                  pass
+
+              # Append JSON
+              properties_found_list.append(property_obj)
+          
+
+          # Find next page
+          next_page = soup.find_all('li', class_ = 'pagination__page-next')
+
+          try:
+              print('next_Page')
+              url_slug = next_page[0].find('a',rel='next',href=True)
+              if url_slug:
+                  url = f'https://www.argenprop.com{url_slug["href"]}'
+                  print(f'Next page is: {url}')
+                  soup = super().get_soup(user_agent,url)
+              else:
+                  print("No next page")
+                  break
+          except Exception as inst:
+              print(f"Error : {inst}")
+              break
+          
+          if counter == max_pages:
+              print(f'Automatically stopping page iteration at : {counter}')
+              break
+          else:
+              counter = counter + 1
+
+      return properties_found_list
--- main.py
+++ main.py
@@ -2,16 +2,18 @@
 # pip3 install lxml
 
 from requests.api import head
-import argenprop_scraper as aps
 import pandas as pd
+from classes.ScraperInterface import ArgenpropScraper
 
 if __name__ == "__main__":
     #set soup
     user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'
     site = 'https://www.argenprop.com/inmuebles-pais-argentina'
 
-    soup = aps.get_soup(user_agent,site)
-    property_list = aps.get_property_list(soup, user_agent)
+    argScraper = ArgenpropScraper()
+    
+    soup = argScraper.get_soup(user_agent,site)
+    property_list = argScraper.get_property_list(soup, user_agent)
 
     prop_list_to_dict = []
     [prop_list_to_dict.append(ob.__dict__) for ob in property_list]
